cd08e26970b9175a9d8c1c3d1480c9b738761db6
dror tartakovsky <dror.tartakovsky@s.afeka.ac.il>
2021-05-11 22:43:23 +0300

    adds put api to users

--- routes/api/UserApi.js
+++ routes/api/UserApi.js
@@ -31,6 +31,43 @@ router.post("/add", function (req, res, next) {
 });
 
 // update user
+router.put("/:user_name/:email", (req, res, next) => {
+  var key = req.params.user_name + "@@" + req.params.email;
+  client.exists(key, (err, object) => {
+    if (object === 1) {
+      let user_name = req.body.user_name;
+      let email = req.body.email;
+      let role = req.body.role;
+      client.hmset(
+        key,
+        { user_name: user_name, email: email, role: role },
+        (err, object) => {
+          if (object === "OK") {
+            res.send("user updated");
+          } else {
+            res.send(err);
+          }
+        }
+      );
+    } else {
+      let user_name = req.body.user_name;
+      let email = req.body.email;
+      let role = req.body.role;
+      let id = user_name + "@@" + email;
+      client.hmset(
+        id,
+        ["user_name", user_name, "email", email, "role", role],
+        function (err, reply) {
+          if (err) {
+            console.log(err);
+          }
+          console.log(reply);
+          res.send(req.body);
+        }
+      );
+    }
+  });
+});
 
 // Delete User
 router.delete("/delete/:key", function (req, res, next) {
