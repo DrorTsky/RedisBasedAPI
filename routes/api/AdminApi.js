e6d12c5fc76b1ab48f35d0549657c5fda752b277
dror tartakovsky <dror.tartakovsky@s.afeka.ac.il>
2021-05-11 22:21:03 +0300

    creates admin api's

---
+++ routes/api/AdminApi.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+var async = require("async");
+let client = require("../../redis-db");
+
+router.get("/users/:id", async (req, res, next) => {
+  client.keys("*", async (err, keys) => {
+    if (keys) {
+      var users = {};
+      keys.forEach((key, index, array) => {
+        client.hgetall(key, (err, object) => {
+          users[key] = object;
+          if (array.length === Object.keys(users).length) {
+            res.send(users);
+          }
+        });
+      });
+    } else {
+      res.send("empty database");
+    }
+  });
+});
+
+router.delete("/users/:id", (req, res, next) => {
+  client.keys("*com", (err, keys) => {
+    if (keys.length !== 0) {
+      keys.forEach((key, index, array) => {
+        client.del(key, () => {
+          if (index + 1 === array.length) res.send("users were deleted");
+        });
+      });
+    } else res.send("no users");
+  });
+});
+
+router.delete("/flushall", (req, res, next) => {
+  client.flushall(() => {
+    res.send("deleted all data");
+  });
+});
+
+module.exports = router;
